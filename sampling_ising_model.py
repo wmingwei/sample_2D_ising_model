665d9f988d17b14f2ed9113460b3263d59fa0fd0
wmingwei <wmingweiw@gmail.com>
2017-06-22 13:33:49 -0500

    update sampling function

--- sampling_ising_model.py
+++ sampling_ising_model.py
@@ -6,11 +6,9 @@ import multiprocessing
 
 Size = 40
 J = 1
-H = 0.0
-Temp = 0
 
-def energy(field):
-    energy = H
+def energy(field, J):
+    energy = 0.0
     size = len(field)
     for x in range(size):
         for y in range(size):
@@ -21,7 +19,7 @@ def energy(field):
     return energy
 
 
-def specific_heat(samples, temp, parallel = False):
+def specific_heat(samples, temp, J, parallel = False):
     
     samples_energy = []
     if not parallel:
@@ -35,8 +33,8 @@ def specific_heat(samples, temp, parallel = False):
     
     return 1/temp**2*((samples_energy**2).mean()-(samples_energy.mean())**2)
 
-def spin_direction(field, x, y, Temp):
-    energy = H
+def spin_direction(field, x, y, Temp, J):
+    energy = 0.0
     size = len(field)
     for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
         energy += - J * field[(x + dx)%size][(y + dy)%size]
@@ -53,36 +51,29 @@ def spin_direction(field, x, y, Temp):
 
 
 
-def run_gibbs_sampling(field, Temp, seed, iternum):
+def run_gibbs_sampling(field, Temp, seed, iternum, J):
     np.random.seed(seed)
     for _ in range(iternum):
         lattice = [(x,y) for x in range(Size) for y in range(Size)]
         np.random.shuffle(lattice)
         for x, y in lattice:
-            field[x][y] = spin_direction(field, x, y, Temp)
+            field[x][y] = spin_direction(field, x, y, Temp, J = J)
             
             
-def sampling(temps, seed, iternum = 20):
+def sampling(temps, seed, J, iternum = 100):
     np.random.seed(seed)
     samples = []
     field = randint(2,size=(Size,Size))*2-1
     for temp in temps:
-        run_gibbs_sampling(field = field, Temp = temp, seed = seed, iternum = iternum)
+        run_gibbs_sampling(field = field, Temp = temp, seed = seed, iternum = iternum, J = J)
         samples.append(list(field.copy().reshape(Size*Size)) + [temp])
     return samples
 
-num_cores = multiprocessing.cpu_count()
 
-results = Parallel(n_jobs=num_cores)(delayed(sampling)(temps = temps, seed = None) for i in range(100))
-results = np.array(results)
-results = results.reshape(100*27, 1600+1)
-np.savetxt('samples.csv', results, delimiter=",",fmt='%f')
-for j in range(9):
-    results = Parallel(n_jobs=num_cores)(delayed(sampling)(temps = temps, seed = None) for i in range(100))
+def get_samples(temps, size = Size, num_samples = 1000, iternum = 100):
+    num_cores = multiprocessing.cpu_count()
+
+    results = Parallel(n_jobs=num_cores)(delayed(sampling)(temps = temps, seed = None, J = J, iternum = iternum) for i in range(num_samples))
     results = np.array(results)
-    results = results.reshape(100*27, 1600+1)
-    samples = list(np.genfromtxt('samples.csv', delimiter=","))
-    samples += list(results)
-    samples = np.array(samples)
-    np.savetxt('samples.csv', samples, delimiter=",",fmt='%f')
-    print(j)
+    results = results.reshape(num_samples*len(temps), size*size+1)
+    np.savetxt('samples.csv', results, delimiter=",",fmt='%f')
